395535282ca72fa7c68238ca08f12d2425e03c46
Aleksei Klymenok <klymenok.a@gmail.com>
2017-05-17 15:42:13 +0300

    main header file

---
+++ Corewar_vm/Corewar_vm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Corewar_vm
+//
+//  Created by Oleksij Klymenok on 5/17/17.
+//  Copyright © 2017 Oleksij Klymenok. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
