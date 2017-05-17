88439f25f681e5926e89b698ef94d5cb56e655bf
Aleksei Klymenok <klymenok.a@gmail.com>
2017-05-17 16:10:43 +0300

    h files for controller

---
+++ includes/controller.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   head.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oklymeno <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/17 16:04:13 by oklymeno          #+#    #+#             */
+/*   Updated: 2017/05/17 16:05:09 by oklymeno         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTROLLER_H
+# define CONTROLLER_H
+
+# include "libft.h"
+# include "op.h"
+
+//validation functions
+int	check_magic(int magic);
+int	read_file_vm(char *file);
+
+
+#endif
