d1171c34f6fc8071cfb3f12efc8d58c8b9d75cc7
abaranov <abaranov@student.unit.ua>
2017-05-19 19:58:09 +0300

    git ign

--- cmake-build-debug/CMakeFiles/virtual_machine.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/virtual_machine.dir/cmake_clean.cmake
@@ -1,10 +1,5 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/virtual_machine.dir/main.c.o"
-  "virtual_machine.pdb"
-  "virtual_machine"
-)
 
 # Per-language clean rules from dependency scanning.
-foreach(lang C)
+foreach(lang )
   include(CMakeFiles/virtual_machine.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
