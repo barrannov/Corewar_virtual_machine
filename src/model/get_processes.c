a69eb25b8e0f5258128f1084d10c721045094ac7
Aleksei Klymenok <klymenok.a@gmail.com>
2017-05-25 22:00:01 +0300

    st and processes files

---
+++ src/model/get_processes.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_processes.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oklymeno <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/25 17:34:21 by oklymeno          #+#    #+#             */
+/*   Updated: 2017/05/25 18:49:54 by oklymeno         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/vm_header.h"
+
+void	init_reg(t_processor *proc)
+{
+	int i;
+
+	i = -1;
+	while (++i < REG_NUMBER)
+		proc->reg[i] = 0;
+}
+
+void	add_process(t_player *player, int pc)
+{
+	t_processor	*new;
+	t_processor *tmp;
+
+	new = malloc(sizeof(t_processor) + 1);
+	new->prog_counter = pc;
+	init_reg(new);
+	new->reg[0] = player->numb;
+	new->carry = 0;
+	new->waite_cycles = 0;
+	new->next = NULL;
+	tmp = player->processors;
+	if (!player->processors)
+		player->processors = new;
+	else
+	{
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = new;
+	}
+}
+
+void	get_processes(t_param *param)
+{
+	t_player	*tmp;
+
+	tmp = param->players;
+	while (tmp)
+	{
+		add_process(tmp, 0);
+		tmp = tmp->next;
+	}
+}
