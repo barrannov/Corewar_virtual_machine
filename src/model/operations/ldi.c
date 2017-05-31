735de729af073bff67ad9553aa669bacedebe9ef
Aleksei Klymenok <klymenok.a@gmail.com>
2017-05-31 21:00:25 +0300

    ldi amd live corrections

--- src/model/operations/ldi.c
+++ src/model/operations/ldi.c
@@ -6,41 +6,41 @@
 /*   By: oklymeno <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/28 13:22:58 by oklymeno          #+#    #+#             */
-/*   Updated: 2017/05/29 18:15:36 by oklymeno         ###   ########.fr       */
+/*   Updated: 2017/05/31 20:55:50 by oklymeno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/vm_header.h"
 
-static unsigned int	arg_1(t_param *params, t_processor *proc, t_val *val)
+static int	arg_1(t_param *params, t_processor *proc, t_val *val)
 {
 	if (val->val1 == 1)
-		return ((unsigned int)proc->reg[(proc->pc + 2) % MEM_SIZE]);
+		return (proc->reg[params->map[(proc->pc + 2) % MEM_SIZE] - 1]);
 	else if (val->val1 == 2)
 		return (handle_dir(params, proc, 2, 2));
 	else
 		return (handle_ind(params, proc, 2, 1));
 }
 
-static unsigned int	arg_2(t_param *params, t_processor *proc, t_val *val)
+static int	arg_2(t_param *params, t_processor *proc, t_val *val)
 {
 	char	pos;
 
 	val->val1 == 1 ? (pos = 1) :
 		(pos = 2);
 	if (val->val2 == 1)
-		return ((unsigned int)proc->reg[(proc->pc + 2 + pos) % MEM_SIZE]);
+		return (proc->reg[params->map[(proc->pc + 2 + pos) % MEM_SIZE] - 1]);
 	else
 		return (handle_dir(params, proc, 2, 2 + pos));
 }
 
-void				handle_ldi(t_param *params, t_processor *proc)
+void		handle_ldi(t_param *params, t_processor *proc)
 {
 	t_val			*val;
-	unsigned int	arg1;
-	unsigned int	arg2;
+	int				arg1;
+	int				arg2;
 	static int		move[2] = {0, 0};
-	
+
 	val = malloc(sizeof(t_val));
 	get_args(val, params->map, proc);
 	val->val1 == 1 ? move[0]++ : (move[0] += 2);
@@ -49,14 +49,14 @@ void				handle_ldi(t_param *params, t_processor *proc)
 	{
 		arg1 = arg_1(params, proc, val);
 		arg2 = arg_2(params, proc, val);
-		proc->reg[params->map[(proc->pc + 1 + move[0] + move[1]) % MEM_SIZE] 
-			- 1] = handle_dir(params, proc, REG_SIZE, ((proc->pc +
-						((arg1 + arg2) % IDX_MOD) % MEM_SIZE)));
-		proc->reg[params->map[(proc->pc + 1 + move[0] + move[1]) % MEM_SIZE]
+		proc->reg[params->map[(proc->pc + count_steps(val, 10) - 1) % MEM_SIZE]
+			- 1] = handle_dir(params, proc, REG_SIZE, (((arg1 + arg2)
+							% IDX_MOD) % MEM_SIZE));
+		proc->reg[params->map[(proc->pc + count_steps(val, 10) - 1) % MEM_SIZE]
 			- 1] == 0 ? (proc->carry = 1) :
-		   (proc->carry = 0);
-		proc->pc = (proc->pc + 2 + move[0] + move[1]) % MEM_SIZE;
+			(proc->carry = 0);
+		proc->pc = (proc->pc + count_steps(val, 10)) % MEM_SIZE;
 	}
 	else
-		proc->pc = (proc->pc + 1) % MEM_SIZE;
+		proc->pc = (proc->pc + count_steps(val, 10)) % MEM_SIZE;
 }
--- src/model/operations/live.c
+++ src/model/operations/live.c
@@ -6,7 +6,7 @@
 /*   By: oklymeno <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/29 18:17:30 by oklymeno          #+#    #+#             */
-/*   Updated: 2017/05/29 18:18:50 by oklymeno         ###   ########.fr       */
+/*   Updated: 2017/05/31 19:16:01 by oklymeno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,28 @@
 
 void	live_to_chemp(t_player *players, int arg)
 {
-    t_player	*temp_player;
+	t_player	*temp_player;
 
-    temp_player = players;
-    while (temp_player)
-    {
-        if (temp_player->numb == arg)
-            temp_player->live = 1;
-        else
-            temp_player->live = 0;
-        temp_player = temp_player->next;
-    }
+	temp_player = players;
+	while (temp_player)
+	{
+		if (temp_player->numb == arg)
+		{
+			temp_player->live = 1;
+			temp_player->live_amount++;
+		}
+		else
+			temp_player->live = 0;
+		temp_player = temp_player->next;
+	}
 }
 
 void	handle_live(t_param *params, t_processor *proc)
 {
-    int	arg;
+	int	arg;
 
-    arg = handle_dir(params, proc, 4, 1);
-    live_to_chemp(params->players, arg);
-    proc->is_alive = 1;
-    proc->pc = (proc->pc + 5) % MEM_SIZE;
+	arg = handle_dir(params, proc, 4, 1);
+	live_to_chemp(params->players, arg);
+	proc->is_alive = 1;
+	proc->pc = (proc->pc + 5) % MEM_SIZE;
 }
