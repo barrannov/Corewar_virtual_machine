4ffe03236826fe3345690d21678c05372f034286
Aleksei Klymenok <klymenok.a@gmail.com>
2017-06-06 23:26:42 +0300

    live test

--- src/model/operations/live.c
+++ src/model/operations/live.c
@@ -6,13 +6,13 @@
 /*   By: oklymeno <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/29 18:17:30 by oklymeno          #+#    #+#             */
-/*   Updated: 2017/06/06 21:11:36 by oklymeno         ###   ########.fr       */
+/*   Updated: 2017/06/06 23:15:59 by oklymeno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/vm_header.h"
 
-void	live_to_chemp(t_player *players, int arg, int play)
+void	live_to_chemp(t_player *players, int arg)
 {
 	t_player	*temp_player;
 
@@ -23,8 +23,7 @@ void	live_to_chemp(t_player *players, int arg, int play)
 		{
 			temp_player->live = 1;
 			temp_player->live_amount++;
-			if (play == 1)
-				say_live(temp_player->header->prog_name);
+			say_live(temp_player->header->prog_name);
 		}
 		else
 			temp_player->live = 0;
@@ -32,12 +31,12 @@ void	live_to_chemp(t_player *players, int arg, int play)
 	}
 }
 
-void	handle_live(t_param *params, t_processor *proc, int play)
+void	handle_live(t_param *params, t_processor *proc)
 {
 	int	arg;
 
 	arg = handle_dir(params, proc, 4, 1);
-	live_to_chemp(params->players, arg, play);
+	live_to_chemp(params->players, arg);
 	proc->is_alive++;
 	proc->pc = (proc->pc + 5) % MEM_SIZE;
 }
